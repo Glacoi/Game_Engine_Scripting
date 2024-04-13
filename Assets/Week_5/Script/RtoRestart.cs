c6a467129a6bb61b5280cc4225d5e20fc0e0b67e
Glacoi <123410996+Glacoi@users.noreply.github.com>
2024-04-13 17:53:24 -0500

    Restart

--- Assets/Week_5/Script/RtoRestart.cs
+++ Assets/Week_5/Script/RtoRestart.cs
@@ -1,21 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Restart : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Application.LoadLevel(0);
         }
     }
 }
